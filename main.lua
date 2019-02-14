ee70f53a259b2cd82548847237b821c2016ec2bd
AdenRao3 <47573320+AdenRao3@users.noreply.github.com>
2019-02-14 12:51:40 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,10 +9,22 @@
 --
 -----------------------------------------------------------------------------------------
 
-display.setDefault( "background", 255, 0, 0 )
+local options = 
+{
+    text = "Aden Rao",     
+    x = 150,
+    y = 400,
+    width = 128,
+    font = native.systemFont,   
+    fontSize = 50,
+    align = "center"
+}
 
+local myText = display.newText( options )
+myText:setFillColor( 0, 255, 0 )
+
+display.setDefault( "background", 255, 0, 0 )
+ 
 local image = display.newImageRect( "assets/zombie.png", 250, 250 )
 image.x = 175
-image.y = 250
-
-local myText = display.newText( "Aden Rao", 150, 450, native.systemFont, 50 )
+image.y = 155
